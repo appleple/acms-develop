0e8411c082b91032a9f0b1c489258c792a54ecbb
Konatsu <kona21.web@gmail.com>
2025-01-24 19:05:04 +0900

    fix ユニットスタイルの調整

--- src/develop/tailwind.config.js
+++ src/develop/tailwind.config.js
@@ -52,6 +52,12 @@ module.exports = {
       typography: {
         DEFAULT: {
           css: {
+            h4: {
+              'font-size': '1.125rem',
+            },
+            p: {
+              'line-height': '1.7',
+            },
             a: {
               '&:hover': {
                 'text-decoration': 'none',
@@ -60,6 +66,16 @@ module.exports = {
             hr: false,
             blockquote: {
               'font-style': 'normal',
+            },
+            'ul > li::marker': {
+              'color': '#9CA3AF',
+            },
+            'ol > li::marker': {
+              'font-weight': 'bold',
+            },
+            dd: {
+              'margin-top': '0',
+              'padding-inline-start': '1em',
             },
             table: {
               'overflow': 'hidden',
@@ -88,6 +104,9 @@ module.exports = {
             'tr:last-child th, tr:last-child td': {
               'border-bottom': '0',
             },
+            '[class*="column-media-"] img': {
+              "margin-bottom": ".75em",
+            }
           },
         },
       },
