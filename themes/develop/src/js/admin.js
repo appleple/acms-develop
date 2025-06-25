b2bfba913bf4b6c1489dd2f8d3ab0868b3128a30
菅原彩 <sugawara@appleple.com>
2025-06-25 19:12:59 +0900

    ブロックエディター内要素にスタイル適用、ユニットスタイルをsystem合わせでファイル分割

--- themes/develop/src/js/admin.js
+++ themes/develop/src/js/admin.js
@@ -1,3 +1,8 @@
+/**
+ * スタイルの読み込み
+ */
+import '../style/editor.css';
+
 ACMS.Ready(() => {
   ACMS.Config.LiteEditorFieldConf.btnPosition = 'bottom';
   ACMS.Config.LiteEditorFieldConf.classNames = {
