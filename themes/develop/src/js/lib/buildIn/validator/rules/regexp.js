78084afc5d91311ff4a0d10baf87d54d3e812c95
宇井 陸登 <ui@appleple.com>
2026-09-04 14:30:56 +0900

    フォームバリデーション処理を @ablogcms/validator に共通化
    自前実装の Validator（buildIn/validator 配下）を、a-blog cms 本体が公開した
    @ablogcms/validator パッケージに置き換え、コア側と実装を共通化する。

--- themes/develop/src/js/lib/buildIn/validator/rules/regexp.js
+++
@@ -1,9 +0,0 @@
-/**
- * Alias for regex validation rule
- * @type {import('../types').ValidationRule}
- */
-import { regex } from './regex';
-
-export const regexp = (val, pattern, input, v) => {
-  return regex(val, pattern, input, v);
-};
